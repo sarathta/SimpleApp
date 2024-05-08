b0fc38847f6cd7662774088ef340a55d2366ceac
sarath <sarath@aicnet.in>
2024-05-08 11:27:21 +0530

    End of Section 5

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader()
 .AllowAnyMethod().WithOrigins("https://localhost:4200"));
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection(); // not important
 app.UseAuthentication();
 app.UseAuthorization(); 
 
